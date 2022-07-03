6f044d48d766cb285c103fab983b17d26f705418
Douglas Melo <dj.eternally@live.com>
2022-07-03 17:47:41 -0300

    continuidade do dia 1

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://dmelo:<8k8n36ng>@slumfood.hfoax.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+})
+
+// GET= BUSCAR UMA INFOR, POST= CRIAR, PUT=EDITAR, DELETE
+// req.query = Acessar query params
+// req.params = acessar route params (para edição,delete)
+// req.body = acessar corpo da requisição (para criaçao e ediçao)
+// sequelize banco de dados
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);

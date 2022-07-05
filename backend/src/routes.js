5ddfc9ea9f8174fbdb12f0e131c2e20e77505ab8
Douglas Melo <dj.eternally@live.com>
2022-07-05 18:58:02 -0300

    criando os spots

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
 
 const routes = express.Router();
 
 routes.post('/sessions', SessionController.store);
 
-  module.exports = routes;
+routes.post('/spots', SpotController.store);
+  
+
+module.exports = routes;
 
 
   /* (req, res) => {
